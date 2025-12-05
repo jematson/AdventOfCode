747e539bb7a9a2cdf150291d81e0fc03fb72a773
Jenae Matson <jenaematson101@gmail.com>
2025-12-04 19:55:16 -0900

    Remove day folders, create 2025 day 5

---
+++ 2025/day5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void part1() {
+
+    string input;
+    ifstream File("input.txt");
+
+    while(getline(File, input)) {
+
+    }
+
+    cout << "Part 1 Answer: " << "" << endl;
+
+}
+
+void part2() {
+
+    string input;
+    ifstream File("input.txt");
+
+    while(getline(File, input)) {
+
+    }
+
+    cout << "Part 2 Answer: " << "" << endl;
+}
+
+int main() {
+
+    part1();
+    part2();
+
+    return 0;
+}
