79e69398f6a66490b9a6e5287d53d75e83e28093
Jenae Matson <jenaematson101@gmail.com>
2025-12-07 19:54:31 -0900

    2025 day 7 part 1

--- 2025/day7.cpp
+++ 2025/day7.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,11 +15,43 @@ void part1() {
     string input;
     ifstream File("input.txt");
 
+    // Store file input;
+    vector<vector<char>> graph;
+    int index = 0;
     while(getline(File, input)) {
-
+        graph.push_back({});
+        for(int i=0; i<input.length(); i++) {
+            char c = input[i];
+            graph[index].push_back(c);
+        }
+        index++;
     }
 
-    cout << "Part 1 Answer: " << "" << endl;
+    vector<int> beams(graph[0].size(), 0);
+
+    size_t splits = 0;
+
+    for(int line=0; line < graph.size(); line++) {
+        for(int col=0; col < graph[0].size(); col++) {
+
+            char c = graph[line][col];
+
+            // Start of tachyon beam
+            if (c == 'S') {
+                beams[col] = 1;
+            }
+
+            // Beam split
+            if (c == '^' && beams[col] == 1) {
+                splits++;
+                beams[col] = 0;
+                beams[col+1] = 1;
+                beams[col-1] = 1;
+            }
+        }
+    }
+
+    cout << "Part 1 Answer: " << splits << endl;
 
 }
 
@@ -27,7 +60,38 @@ void part2() {
     string input;
     ifstream File("input.txt");
 
+    // Store file input;
+    vector<vector<char>> graph;
+    int index = 0;
     while(getline(File, input)) {
+        graph.push_back({});
+        for(int i=0; i<input.length(); i++) {
+            char c = input[i];
+            graph[index].push_back(c);
+        }
+        index++;
+    }
+
+    vector<int> beams(graph[0].size(), 0);
+    size_t timelines = 0;
+
+    for(int line=0; line < graph.size(); line++) {
+        for(int col=0; col < graph[0].size(); col++) {
+
+            char c = graph[line][col];
+
+            // Start of tachyon beam
+            if (c == 'S') {
+                beams[col] = 1;
+            }
+
+            // Beam split
+            if (c == '^' && beams[col] == 1) {
+                beams[col] = 0;
+                beams[col+1] = 1;
+                beams[col-1] = 1;
+            }
+        }
 
     }
 
